68682f1c92663ac7d3c2c39dfe07909cd4545a5c
Dennis Chuah <dennis@inetgardens.com>
2019-10-01 17:31:40 +1300

    Wired up ViewModel

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using TestWPFCore30.ViewModel;
 
 namespace TestWPFCore30 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        public GameViewModel ViewModel { get; } = new GameViewModel ();
+
         public MainWindow () {
+            DataContext = this;
             InitializeComponent ();
+        }
+
+        private void StartGame (object _, RoutedEventArgs __) {
+            ViewModel.Start ();
+        }
+
+        private void StopGame (object _, RoutedEventArgs __) {
+            ViewModel.Stop ();
         }
     }
 }
