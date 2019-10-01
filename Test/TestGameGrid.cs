cddac0d814408625570b11897005f62da6094b67
Dennis Chuah <dennis@inetgardens.com>
2019-10-01 17:22:11 +1300

    Added unit tests

---
+++ Test/TestGameGrid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using TestWPFCore30.Model;
+
+namespace Test {
+    [TestFixture]
+    public class TestGameGrid {
+        [Test]
+        public void TestGrid () {
+            GameGrid grid = new GameGrid (10, 10, 100); // All alive!
+            IEnumerable<Cell> cells = grid.GetCells ().SelectMany (row => row);
+            Assert.AreEqual (100, cells.Count ());
+            Assert.AreEqual (100, cells.Where (cell => cell.IsAlive).Count ());
+            grid.NextIteration (); // Special case, only 4 corner cells will be left alive
+            cells = grid.GetCells ().SelectMany (row => row);
+            Assert.AreEqual (4, cells.Where (cell => cell.IsAlive).Count ());
+            grid = new GameGrid (10, 10, 0); // All dead!
+            cells = grid.GetCells ().SelectMany (row => row);
+            Assert.AreEqual (100, cells.Count ());
+            Assert.AreEqual (0, cells.Where (cell => cell.IsAlive).Count ());
+        }
+    }
+}
---
+++ Test/TestGameViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using NUnit.Framework;
+using TestWPFCore30.ViewModel;
+
+namespace Test {
+    [TestFixture]
+    public class TestGameViewModel {
+        [Test]
+        public void TestSize() {
+            GameViewModel viewModel = new GameViewModel ();
+            Assert.Throws<ArgumentException> (() => { viewModel.Width = 5; });
+            Assert.Throws<ArgumentException> (() => { viewModel.Height = 9; });
+            Assert.DoesNotThrow (() => { viewModel.Width = 15; });
+            Assert.DoesNotThrow (() => { viewModel.Height = 11; });
+        }
+
+        [Test]
+        public void TestStart () {
+            GameViewModel viewModel = new GameViewModel {
+                Width = 10,
+                Height = 10,
+                InitialAlivePercentage = 0 // All dead
+            };
+            viewModel.Start ();
+            viewModel.Stop ();
+            Assert.AreEqual (
+                "                    \r\n" +
+                "                    \r\n" +
+                "                    \r\n" +
+                "                    \r\n" +
+                "                    \r\n" +
+                "                    \r\n" +
+                "                    \r\n" +
+                "                    \r\n" +
+                "                    \r\n" +
+                "                    ",
+                viewModel.Display);
+            viewModel = new GameViewModel {
+                Width = 10,
+                Height = 10,
+                InitialAlivePercentage = 100 // All alive
+            };
+            viewModel.PropertyChanged += ViewModel_PropertyChanged;
+            viewModel.Start ();
+            Assert.AreEqual (
+                "####################\r\n" +
+                "####################\r\n" +
+                "####################\r\n" +
+                "####################\r\n" +
+                "####################\r\n" +
+                "####################\r\n" +
+                "####################\r\n" +
+                "####################\r\n" +
+                "####################\r\n" +
+                "####################",
+                viewModel.Display);
+        }
+
+        private void ViewModel_PropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+            if (sender is GameViewModel viewModel) {
+                viewModel.Stop ();
+                Assert.AreEqual (
+                    "##                ##\r\n" +
+                    "                    \r\n" +
+                    "                    \r\n" +
+                    "                    \r\n" +
+                    "                    \r\n" +
+                    "                    \r\n" +
+                    "                    \r\n" +
+                    "                    \r\n" +
+                    "                    \r\n" +
+                    "##                ##",
+                    viewModel.Display);
+            }
+        }
+    }
+}
